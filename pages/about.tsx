27aff83fca8105a3cd574afadb835dc754d3f1a0
dhzdhd <ragas5432100@gmail.com>
2022-07-28 10:05:40 +0530

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,10 +4,11 @@ import Image from 'next/image';
 
 const About: NextPage = () => {
   return (
-    <div className='grid m-auto text-center place-self-center dark:text-white'>
-      <p className='text-3xl md:text-5xl lg:text-7xl'>
-        Our vision: To reshape MIT Manipal&apos;s brand image in academics and to
-        develop a holistic learning community within MIT Manipal
+    <div className='grid m-auto text-center place-self-center gap-10 dark:text-white'>
+      <h1 className='text-3xl md:text-5xl lg:text-7xl font-bold'>Our vision</h1>
+      <p className='text-2xl md:text-4xl lg:text-6xl'>
+        To reshape MIT Manipal&apos;s brand image in academics and
+        to develop a holistic learning community within MIT Manipal
       </p>
     </div>
   );
