a6f192a0d1f9f89396e59f4929c52eec0db802f9
dhzdhd <ragas5432100@gmail.com>
2022-08-03 01:46:05 +0530

    Fix import error

--- frontend/components/BlogCard.tsx
+++ frontend/components/BlogCard.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { FunctionComponent } from 'react';
 import { serverUrl } from '../constants';
-import { BlogPost } from '../pages/blog';
+import type { BlogPost } from '../services/api';
 
 interface BlogCardProps {
   data: BlogPost;
