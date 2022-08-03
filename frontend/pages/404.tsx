6af0499182aa321a2e6b28be6520ea0ca22680c8
dhzdhd <ragas5432100@gmail.com>
2022-08-03 11:29:45 +0530

    Update error routes

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -2,9 +2,9 @@ import type { NextPage } from 'next';
 
 const ClientErrorPage: NextPage = () => {
   return (
-    <div className='grid self-center m-auto text-center'>
-      <h1 className=' text-7xl'>404</h1>
-      <h2 className='text-4xl'>Page not found</h2>
+    <div className='grid self-center gap-5 m-auto text-center dark:text-white'>
+      <h1 className='font-bold text-7xl'>404</h1>
+      <h2 className='text-6xl'>Page not found</h2>
     </div>
   );
 };
--- frontend/pages/500.tsx
+++ frontend/pages/500.tsx
@@ -2,9 +2,9 @@ import type { NextPage } from 'next';
 
 const ServerErrorPage: NextPage = () => {
   return (
-    <div className='grid self-center m-auto text-center'>
-      <h1 className=' text-7xl'>500</h1>
-      <h2 className='text-4xl'>Server Error</h2>
+    <div className='grid self-center gap-5 m-auto text-center dark:text-white'>
+      <h1 className='font-bold text-7xl'>500</h1>
+      <h2 className='text-6xl'>Server error</h2>
     </div>
   );
 };
