124ca45620227b92b2bd457bd51b80264064b9ee
dhzdhd <ragas5432100@gmail.com>
2022-09-08 12:50:15 +0530

    Configure db config

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,15 +1,19 @@
 export default ({ env }: {env: any}) => ({
-  forceMigration: true,
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'postgres'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
-      ssl: env('DATABASE_SSL', false),
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
+      },
+      options: {
+        ssl: false,
+      },
     },
-    debug: false,
   },
 });
