d403cc90f6b9ad73f61c22492f1f9e3697a97b07
dhzdhd <ragas5432100@gmail.com>
2022-09-14 12:12:43 +0530

    Remove firebase

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,25 +1,9 @@
 import express from "express";
 import payload from "payload";
-// import { initializeApp } from 'firebase/app';
-// import { getStorage, ref } from "firebase/storage";
 
 require("dotenv").config();
 const app = express();
 
-// const firebaseConfig = {
-//   apiKey: process.env.APIKEY,
-//   authDomain: "osf-cms.firebaseapp.com",
-//   projectId: "osf-cms",
-//   storageBucket: "osf-cms.appspot.com",
-//   messagingSenderId: process.env.MESSAGINGSENDERID,
-//   appId: process.env.APPID,
-//   measurementId: process.env.MEASUREMENTID,
-// };
-
-// const firebaseApp = initializeApp(firebaseConfig);
-// const storage = getStorage(firebaseApp);
-
-// Redirect root to Admin panel
 app.get("/", (_, res) => {
   res.redirect("/admin");
 });
@@ -33,7 +17,5 @@ payload.init({
     payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
   },
 });
-
-// Add your own express routes here
 
 app.listen(3000);
