ee69f122cace5c528ffa1c2cebb468b7527114cd
dhzdhd <ragas5432100@gmail.com>
2022-10-14 13:27:58 +0530

    Add custom animations

--- frontend/components/BoardCarousel.tsx
+++ frontend/components/BoardCarousel.tsx
@@ -1,6 +1,6 @@
 import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useState, forwardRef, Dispatch, SetStateAction } from 'react';
+import { useState, forwardRef, Dispatch, SetStateAction, useEffect, Children, ReactNode, FC, useRef, createRef, useLayoutEffect } from 'react';
 import FlipMove from 'react-flip-move';
 import boardData from '../public/boardData.json';
 
@@ -68,6 +68,73 @@ const rotateLeft = (
   setBoard(temp);
 };
 
+function usePrevious(value: any) {
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+
+  return ref.current;
+}
+
+const calculateBoundingBoxes = (children: any) => {
+  const boundingBoxes: any = {};
+
+  Children.forEach(children, child => {
+    const domNode = child.ref.current;
+    const nodeBoundingBox = domNode.getBoundingClientRect();
+
+    boundingBoxes[child.key] = nodeBoundingBox;
+  });
+
+  return boundingBoxes;
+};
+
+const AnimateChildren = ({children}: {children: any}) => {
+  const [boundingBox, setBoundingBox]: any = useState({});
+  const [prevBoundingBox, setPrevBoundingBox]: any = useState({});
+  const prevChildren = usePrevious(children);
+
+  useLayoutEffect(() => {
+    const newBoundingBox = calculateBoundingBoxes(children);
+    setBoundingBox(newBoundingBox);
+  }, [children]);
+
+  useLayoutEffect(() => {
+    const prevBoundingBox = calculateBoundingBoxes(prevChildren);
+    setPrevBoundingBox(prevBoundingBox);
+  }, [prevChildren]);
+
+  useLayoutEffect(() => {
+    const hasPrevBoundingBox = Object.keys(prevBoundingBox).length;
+
+    if (hasPrevBoundingBox) {
+      Children.forEach(children, child => {
+        // TODO: Add support for more than 5 members
+        const domNode = child.ref.current;
+        const firstBox = prevBoundingBox[child.key];
+        const lastBox = boundingBox[child.key];
+        const changeInX = firstBox.left - lastBox.left;
+        const changeInY = firstBox.bottom - lastBox.bottom;
+
+        if (changeInX) {
+          requestAnimationFrame(() => {
+            domNode.style.transform = `translateY(${changeInY}px) translateX(${changeInX}px)`;
+            domNode.style.transition = "transform 0s";
+
+            requestAnimationFrame(() => {
+              domNode.style.transform = "";
+              domNode.style.transition = "transform 500ms";
+            });
+          });
+        }
+      });
+    }
+  }, [boundingBox, prevBoundingBox, children]);
+
+  return children;
+}
 
 const BoardCarousel = () => {
   const [board, setBoard] = useState(boardData);
@@ -76,8 +143,8 @@ const BoardCarousel = () => {
     <div className='grid grid-cols-5 gap-10'>
       {/* FIXME: https://github.com/joshwcomeau/react-flip-move/issues/273 */}
       {/* <FlipMove className='grid grid-cols-5 gap-10'> */}
+      <AnimateChildren>
       {board.slice(0, 5).map((value, index) => {
-        // console.log(JSON.stringify(value));
         if (index === 2) {
           return (
             <MiddleBoardCarouselItem
@@ -86,13 +153,15 @@ const BoardCarousel = () => {
               value={value}
               board={board}
               setBoard={setBoard}
+              ref={createRef()}
             />
           );
         }
         return (
-          <BoardCarouselItem key={value.key} index={index} value={value} />
+          <BoardCarouselItem key={value.key} index={index} value={value} ref={createRef()} />
         );
       })}
+      </AnimateChildren>
       {/* </FlipMove> */}
     </div>
   );
@@ -114,12 +183,15 @@ const MiddleBoardCarouselItem = forwardRef<HTMLDivElement, MiddleProps>(
             <FontAwesomeIcon size='lg' icon={faArrowLeft}></FontAwesomeIcon>
           </button>
 
-          <div ref={ref} className={`avatar ${getPlacement(props.index)}`}>
+          <div ref={ref} key={props.value.key} className={`avatar ${getPlacement(props.index)}`}>
             <div
               className={`
                    mask mask-squircle ${getStyle(props.index)}`}
             >
               <img alt='e' src={props.value.url} />
+              <div>
+
+              </div>
             </div>
           </div>
 
