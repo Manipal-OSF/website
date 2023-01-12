dfd8a3b2779bbab83235e3c8c5f0213041a23cef
dhzdhd <ragas5432100@gmail.com>
2023-01-12 12:32:52 +0530

    Fix env vars in frontend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost', process.env.SERVER_URL],
+    domains: ['localhost', process.env.NEXT_PUBLIC_SERVER_URL.substring(8)], // Remove https://
   },
 };
 
