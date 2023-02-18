27805da29e6496a6dc67ddb857de5b24bf9a26c9
dhzdhd <ragas5432100@gmail.com>
2023-02-18 13:24:14 +0530

    Fix line heights

--- frontend/components/RichTextRenderer.tsx
+++ frontend/components/RichTextRenderer.tsx
@@ -57,7 +57,7 @@ const serialize = (children: any) =>
       case 'h1':
         return (
           <Fragment key={i}>
-            <h1 className='text-[3rem]' key={i}>
+            <h1 className='text-[3rem] leading-tight' key={i}>
               {serialize(node.children)}
             </h1>
             <hr className='opacity-30' />
@@ -65,31 +65,31 @@ const serialize = (children: any) =>
         );
       case 'h2':
         return (
-          <h2 className='text-[2.6rem]' key={i}>
+          <h2 className='text-[2.6rem] leading-tight' key={i}>
             {serialize(node.children)}
           </h2>
         );
       case 'h3':
         return (
-          <h3 className='text-[2.3rem]' key={i}>
+          <h3 className='text-[2.3rem] leading-tight' key={i}>
             {serialize(node.children)}
           </h3>
         );
       case 'h4':
         return (
-          <h4 className='text-[2rem]' key={i}>
+          <h4 className='text-[2rem] leading-tight' key={i}>
             {serialize(node.children)}
           </h4>
         );
       case 'h5':
         return (
-          <h5 className='text-[1.6rem]' key={i}>
+          <h5 className='text-[1.6rem] leading-tight' key={i}>
             {serialize(node.children)}
           </h5>
         );
       case 'h6':
         return (
-          <h6 className='text-[1.2rem]' key={i}>
+          <h6 className='text-[1.3rem] leading-tight' key={i}>
             {serialize(node.children)}
           </h6>
         );
@@ -122,7 +122,7 @@ const serialize = (children: any) =>
 
       default:
         return (
-          <p className='text-[1.2rem]' key={i}>
+          <p className='text-[1.2rem] leading-tight' key={i}>
             {serialize(node.children)}
           </p>
         );
