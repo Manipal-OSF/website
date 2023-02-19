fd3161c2d4bd74a1fee3cc4ecfafe4892df7e7ca
Rudeus <71289117+Crimson-Blade@users.noreply.github.com>
2023-02-19 19:44:35 +0530

    Fix alignment issue on landing page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
       </Head>
       <div className='grid m-auto gap-5 text-center place-self-center text-secondary dark:text-secondary-dark'>
         <h2 className='duration-300 text-left italic hover:-translate-y-1 text-accent dark:text-accent-dark'>Experience</h2>
-        <h1 className='text-7xl'>Manipal OSF</h1>
+        <h1 className='text-5xl sm:text-7xl'>Manipal OSF</h1>
       </div>
     </>
   );
