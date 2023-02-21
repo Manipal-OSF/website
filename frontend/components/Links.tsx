bf50dfd7a749afedb2d933134cea16f9a88a7979
Rudeus <71289117+Crimson-Blade@users.noreply.github.com>
2023-02-21 20:18:46 +0530

    Fix navbar underline Issue

--- frontend/components/Links.tsx
+++ frontend/components/Links.tsx
@@ -27,7 +27,7 @@ const NavLink  = ({selectedRoute,i,route}: navLinkProp): ReactElement => {
       <Link href={`/${route.toString().toLowerCase() === "home" ? "/" : route.toString().toLowerCase() }`} scroll={false}>
           <a
             className={`link underline-offset-2 ${
-              selectedRoute === route ? 'decoration-2' : 'link-hover'
+              Route[selectedRoute] === route ? 'decoration-2' : 'link-hover'
             }`}
             >
             {route.toString()}
